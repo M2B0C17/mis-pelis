103ffb5f1e597aa48374b4a7e2d13f3ae5d30b8a
Anayn Pavez <nanyn.pavez@gmail.com>
2017-08-11 12:21:28 -0300

    modificación de estilos y funciones mimificado

--- dist/js/script.js
+++ dist/js/script.js
@@ -12630,6 +12630,66 @@ if (typeof jQuery === 'undefined') {
 
 }(jQuery);
 
+$(document).ready(function() {
+	
+	$('input').change(function(){
+		nombre();
+		contra();
+	});
+
+	$('.login__btn').click(function(e) {
+
+		if( nombre() && contra() ){
+			e.preventDefault();
+			window.location.href = "movie.html";
+		}
+
+	});
+	
+});
+
+
+var nombre = function(){
+	var nom = $('#nombreLogin').val();
+
+	if(nom == ""){
+		$('#nombre-login').addClass('has-error has-feedback');
+		$('#nombre-login').append('<span class="form-control-feedback error-nom" aria-hidden="true"><i class="fa fa-times-circle" aria-hidden="true"></i></span><span id="error-login" class="sr-only error-nom">(error)</span>');
+		$('#nombreLogin').attr('aria-describedby', 'error-login');
+		return false;
+	} 
+	if(!nom == ""){
+		$('#nombre-login').removeClass('has-error').addClass('has-success');
+		$('span').remove('.error-nom');
+		$('#nombreLogin').removeAttr('aria-describedby', 'error-login');
+		$('#nombre-login').append('<span class="form-control-feedback" aria-hidden="true"><i class="fa fa-check-circle" aria-hidden="true"></i></span><span id="correcto-login" class="sr-only ">(success)</span>');
+		$('#nombreLogin').attr('aria-describedby', 'correcto-login');
+		return true;
+	}
+};
+
+var contra = function(){
+	var con = $('#claveLogin').val();
+
+	if(con == ""){
+		$('#contra-login').addClass('has-error has-feedback');
+		$('#contra-login').append('<span class="form-control-feedback error-con" aria-hidden="true"><i class="fa fa-times-circle" aria-hidden="true"></i></span><span id="error-login" class="sr-only error-con">(error)</span>');
+		$('#claveLogin').attr('aria-describedby', 'error-con');
+		return false;
+	} 
+	if(!con == ""){
+		$('#contra-login').removeClass('has-error').addClass('has-success');
+		$('span').remove('.error-con');
+		$('#nombreLogin').removeAttr('aria-describedby', 'error-login');
+		$('#contra-login').append('<span class="form-control-feedback" aria-hidden="true"><i class="fa fa-check-circle" aria-hidden="true"></i></span><span id="correcto-login" class="sr-only">(success)</span>');
+		$('#nombreLogin').attr('aria-describedby', 'correcto-login');
+		return true;
+	}
+};
+
+  						
+
+  						
 
 /* Parametros */
 var actorsAux = [
@@ -12676,7 +12736,7 @@ var addPeliculaEnPantalla = function(movie) {
                         <h4>${movie.show_title} <span> ${movie.release_year} | ${movie.category}</span></h4>
                     </div>
                     <div class="col-xs-3">
-                        <a class="btn">ADD FAVOURITE</a>
+                        <a class="btn-">ADD FAVOURITE</a>
                     </div>
                 </div>
             </div>
